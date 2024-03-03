47707ee366e3635397a24609fb2cdada211cc387
Kushagra Patidar <kushagraptdr18@gmail.com>
2024-03-03 12:14:45 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,160 @@
+
+
+
+var insta=[
+{username:"sagar_kumar18",dp:"https://images.unsplash.com/photo-1599475735868-a8924c458792?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aGFuZHNvbWUlMjBib3l8ZW58MHx8MHx8fDA%3D",story:"https://plus.unsplash.com/premium_photo-1687371769663-f83fef44a72a?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aGFuZHNvbWUlMjBib3l8ZW58MHx8MHx8fDA%3D",profile:"https://images.unsplash.com/photo-1609081524998-a1163e2d44cb?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGhhbmRzb21lJTIwYm95fGVufDB8fDB8fHww"},
+{username:"sunny_kushwah19",dp:"https://images.unsplash.com/photo-1542142430-6c91a7c123b5?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGhhbmRzb21lJTIwYm95fGVufDB8fDB8fHww",story:"https://plus.unsplash.com/premium_photo-1687914772236-8fd7cc43b145?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8aGFuZHNvbWUlMjBib3l8ZW58MHx8MHx8fDA%3D",profile:"https://images.unsplash.com/photo-1599847022902-f64cc1ae97fd?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGhhbmRzb21lJTIwYm95fGVufDB8fDB8fHww"},
+{username:"Pankaj_kunar28",dp:"https://images.unsplash.com/photo-1599475735868-a8924c458792?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aGFuZHNvbWUlMjBib3l8ZW58MHx8MHx8fDA%3D",story:"https://plus.unsplash.com/premium_photo-1687371769663-f83fef44a72a?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aGFuZHNvbWUlMjBib3l8ZW58MHx8MHx8fDA%3D",profile:"https://images.unsplash.com/photo-1492288991661-058aa541ff43?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGhhbmRzb21lJTIwYm95fGVufDB8fDB8fHww"},
+{username:"Kushagra_ptdr19",dp:"https://images.unsplash.com/photo-1601301043068-01ba29e9bf30?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGhhbmRzb21lJTIwYm95fGVufDB8fDB8fHww",story:"https://plus.unsplash.com/premium_photo-1687914772236-8fd7cc43b145?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8aGFuZHNvbWUlMjBib3l8ZW58MHx8MHx8fDA%3D",profile:"https://images.unsplash.com/photo-1613940102159-1e7a40f2b60e?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aGFuZHNvbWUlMjBib3l8ZW58MHx8MHx8fDA%3D"},
+{username:"Aaditya_kanoje23",dp:"https://images.unsplash.com/photo-1599475735868-a8924c458792?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aGFuZHNvbWUlMjBib3l8ZW58MHx8MHx8fDA%3D",story:"https://plus.unsplash.com/premium_photo-1687371769663-f83fef44a72a?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aGFuZHNvbWUlMjBib3l8ZW58MHx8MHx8fDA%3D",profile:"https://images.unsplash.com/photo-1630563232703-bdd4e2c11e4e?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+{username:"Manvendra_singh20",dp:"https://images.unsplash.com/photo-1601301043068-01ba29e9bf30?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGhhbmRzb21lJTIwYm95fGVufDB8fDB8fHww",story:"https://plus.unsplash.com/premium_photo-1687914772236-8fd7cc43b145?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8aGFuZHNvbWUlMjBib3l8ZW58MHx8MHx8fDA%3D",profile:"https://images.unsplash.com/photo-1619457164717-b11dd65f7ef2?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGhhbmRzb21lJTIwYm95fGVufDB8fDB8fHww"},
+{username:"yash_yadav22",dp:"https://images.unsplash.com/photo-1599475735868-a8924c458792?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aGFuZHNvbWUlMjBib3l8ZW58MHx8MHx8fDA%3D",story:"https://plus.unsplash.com/premium_photo-1687371769663-f83fef44a72a?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aGFuZHNvbWUlMjBib3l8ZW58MHx8MHx8fDA%3D",profile:"https://plus.unsplash.com/premium_photo-1687914772236-8fd7cc43b145?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8aGFuZHNvbWUlMjBib3l8ZW58MHx8MHx8fDA%3D"},
+{username:"Manthan_soni98",dp:"https://images.unsplash.com/photo-1601301043068-01ba29e9bf30?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGhhbmRzb21lJTIwYm95fGVufDB8fDB8fHww",story:"https://plus.unsplash.com/premium_photo-1687914772236-8fd7cc43b145?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8aGFuZHNvbWUlMjBib3l8ZW58MHx8MHx8fDA%3D",profile:"https://images.unsplash.com/photo-1630410464448-0bbbd014d129?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8aGFuZHNvbWUlMjBib3l8ZW58MHx8MHx8fDA%3D"}
+
+
+]
+
+var clutter=""
+
+
+
+insta.forEach(function(elem,idx){
+
+   
+clutter+=`  <div class="story">
+
+<div id="dp">
+        <img id =${idx} src="${elem.dp}" alt="">
+</div>
+
+<h5>${elem.username}</h5>
+
+
+</div>
+`
+
+
+
+})
+
+
+
+var clutter2="";
+insta.forEach(function(elm,idx){
+
+
+clutter2+=`  <div id="profiley">
+<div id="navv">
+    <div class="gola">
+        <img id=${idx} src="${elm.dp}" alt="">
+    </div>
+    <h4>${elm.username}</h4>
+</div>
+
+<img id =${idx} src="${elm.profile}" alt="">
+
+<div id="foot">
+    <div id="foot1">
+    <i class="ri-heart-fill"></i>
+
+        <i class="ri-chat-1-line"></i>
+        <i class="ri-send-plane-line"></i>
+
+    </div>
+    <div id="foot2">
+        <i class="ri-bookmark-line"></i>
+    </div>
+</div>
+
+
+
+</div>
+`
+
+
+
+
+})
+
+
+document.querySelector("#storiya").innerHTML=clutter;
+document.querySelector("#profiles").innerHTML=clutter2;
+
+var storiyan=document.querySelector("#storiya");
+var full=document.querySelector("#full");
+var nam=document.querySelector("#full #navv h4");
+
+var icon=document.querySelector("#full i");
+var load=document.querySelector("#loader #load");
+
+
+
+
+storiyan.addEventListener("click",function(dets){
+
+full.style.display="block";
+full.style.backgroundImage=`url(${insta[dets.target.id].story})`
+console.log(dets.target)
+nam.innerHTML=`${insta[dets.target.id].username}`
+
+var a =0;
+var loaderlenght = setInterval(function(){
+    a++
+     load.style.width = `${a}%`
+ 
+ },30);
+
+
+
+
+icon.addEventListener("click",function(){
+    
+    full.style.display="none";
+    
+    clearInterval(time);
+    clearInterval(loaderlenght);
+    
+
+    
+})
+    
+    var time=setTimeout(function(){
+     full.style.display="none";
+    },3000)
+
+})
+
+var theme=document.querySelector("#card #nav2 #theme");
+var card=document.querySelector("#card")
+var h4=document.querySelectorAll("#profiley #navv h4");
+var img=document.querySelector("#nav img");
+var flaf=0;
+theme.addEventListener("click",function(){
+if(flaf==0){
+    theme.innerHTML=`<i class="ri-lightbulb-fill"></i>`
+    card.style.backgroundColor="rgba(0, 0, 0, 0.729)"
+    card.style.color="white";
+   h4.forEach(function(elm){
+    elm.style.color="white"
+   })
+    img.src=`https://www.bkomstudios.com/wp-content/themes/bkom/assets/img/interface/logo-Instagram.png`
+
+    flaf=1;
+
+}
+else{
+    theme.innerHTML=`<i class="ri-lightbulb-line"></i>`
+    card.style.backgroundColor="white"
+    card.style.color="black"
+    h4.forEach(function(elm){
+        elm.style.color="black"
+       })
+      img.src=`https://logos-world.net/wp-content/uploads/2020/04/Instagram-Logo-2010-2013.png`
+
+    flaf=0
+}
+    
+
+
+
+})
